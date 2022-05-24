4eb1d3831df4d6b25de410f58658107dd03e78db
soniafaiz <soniafaiz@hotmail.co.uk>
2022-05-24 21:42:12 +0100

    Ex 1.9 modified:   mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,14 +1,19 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.7 & 1.8
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+paymentCount = 0
 
 while principal > 0:
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
+    if paymentCount < 12:
+        principal -= 1000
+        total_paid += 1000
+    paymentCount += 1
 
-print('Total paid', round(total_paid,2))
+print('Total paid is {:,.2f} over {} months.'.format(total_paid, paymentCount))
