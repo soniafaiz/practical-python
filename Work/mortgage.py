3d483c41f9d10a01e4fb2ae22024e289a99704de
soniafaiz <soniafaiz@hotmail.co.uk>
2022-05-24 22:08:37 +0100

    Ex 1.9 modified:   mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,6 +1,6 @@
 # mortgage.py
 #
-# Exercise 1.7 & 1.8
+# Exercise 1.7, 1.8, 1.9
 
 principal = 500000.0
 rate = 0.05
@@ -8,12 +8,16 @@ payment = 2684.11
 total_paid = 0.0
 paymentCount = 0
 
+extra_payment_start_month = input('Enter the month to start the extra payment:')
+extra_payment_end_month = input('Enter the month to end the extra payment:')
+extra_payment = input('Enter the extra payment:')
+
 while principal > 0:
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
-    if paymentCount < 12:
-        principal -= 1000
-        total_paid += 1000
+    if (paymentCount >= extra_payment_start_month-1) and (paymentCount < extra_payment_end_month-1):
+        principal -= extra_payment
+        total_paid += extra_payment
     paymentCount += 1
 
 print('Total paid is {:,.2f} over {} months.'.format(total_paid, paymentCount))
