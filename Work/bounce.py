9776b95d13c6f3cdd391939e9055bdee0ce82414
soniafaiz <soniafaiz@hotmail.co.uk>
2022-05-24 20:59:16 +0100

    Ex 1.5 modified:   bounce.py 	Ex 1.6 new file:   sears.py 	Ex 1.7 modified:   mortgage.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100.0      # initial height
+bounceBack = 3/5    # proportion of fall that gives height of next bounce
+bounceCount = 0     # bounce number
+
+while bounceCount <10:
+    bounceCount += 1
+    height *= bounceBack
+    print(bounceCount,round(height,4))
