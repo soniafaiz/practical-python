fadae7c2b0d8133e784cdd901686a39800eaab51
soniafaiz <soniafaiz@hotmail.co.uk>
2022-05-26 11:42:53 +0100

    Ex 2.7 modified:   report.py

--- Work/report.py
+++ Work/report.py
@@ -1,16 +1,55 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.4, 2.6
 import csv
-
+from pprint import pprint
+ 
 def read_portfolio(filename):
-    ''' Computes the total cost (shares*price) of a portfolio file'''
+    ''' Read the portfolios from the filename and return a list of portfolios'''
     portfolio = []
 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
-        headers = next(rows)
+        header = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {header[0]:row[0], header[1]:int(row[1]), header[2]:float(row[2])}
             portfolio.append(holding)
     return portfolio
+
+def read_prices(filename):
+    ''' Read the prices from the filename and return a dictionary of prices '''
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except (ValueError, IndexError):
+                print(f'Cant parse: {row} in file {filename}.')
+    return prices                
+
+def gain_loss(portfolio_file, prices_file):
+    ''' Returns the gain/loss of all the portfolio shares '''
+    price_change = []
+    portfolio_value = 0.0
+    total_gain_loss = 0.0
+
+    portfolio = read_portfolio(portfolio_file)
+    prices = read_prices(prices_file)
+
+    for stock in portfolio:
+        try:
+            stock_value = stock['shares'] * prices[stock['name']]
+            stock_gain_loss = stock_value - stock['shares'] * stock['price']
+            price_change.append( (stock['name'], stock['price'], stock_gain_loss) )
+
+            portfolio_value += stock_value
+            total_gain_loss += stock_gain_loss
+
+        except KeyError:
+            print('Key error: can\'t find ', stock)
+
+    print(f'The current portfolio value is ${portfolio_value:,.2f}')
+    print(f'The total gain/loss is ${total_gain_loss:,.2f}')
+
+    return price_change
