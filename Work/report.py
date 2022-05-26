5d85e159ef15f56dc6f42f67b3d42d6363249bfd
soniafaiz <soniafaiz@hotmail.co.uk>
2022-05-26 15:28:42 +0100

    Ex 2.12 modified:   report.py Formatting challenge

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.4, 2.6, 2.7, 2.9
+# Exercise 2.4, 2.6, 2.7, 2.9, 2.10, 2.11, 2.12
 import csv
  
 def read_portfolio(filename):
@@ -28,7 +28,7 @@ def read_prices(filename):
     return prices                
 
 def make_report(portfolio, prices):
-    ''' Returns the gain/loss of all the portfolio shares '''
+    ''' Returns a list of all the portfolio share details along with the current price '''
     price_change = []
 
     for stock in portfolio:
@@ -41,20 +41,32 @@ def make_report(portfolio, prices):
 
     return price_change
 
-def gain_loss(portfolio_file, prices_file):
-    ''' Returns the gain/loss of all the portfolio shares '''
-    price_change = []
+def print_make_report():
+    ''' Prints the output from the make_report function '''
+    portfolio = read_portfolio('Data/portfolio.csv')
+    prices = read_prices('Data/prices.csv')
+    report = make_report(portfolio, prices)
+
+    header = ('Name', 'Shares', 'Price', 'Change')
+    print('%10s %10s %10s %10s' % header)
+    print('---------- ---------- ---------- ----------')
+
+    for name, share, price, change in report:
+        price_str = '$' + '%0.2f' % price
+        print(f'{name:>10s} {share:>10d} {price_str:>10s} {change:>10.2f}')
+
+def print_gain_loss():
+    ''' Outputs the gain/loss of the portfolio '''
     portfolio_value = 0.0
     total_gain_loss = 0.0
 
-    portfolio = read_portfolio(portfolio_file)
-    prices = read_prices(prices_file)
+    portfolio = read_portfolio('Data/portfolio.csv')
+    prices = read_prices('Data/prices.csv')
 
     for stock in portfolio:
         try:
             stock_value = stock['shares'] * prices[stock['name']]
             stock_gain_loss = stock_value - stock['shares'] * stock['price']
-            price_change.append( (stock['name'], stock['price'], stock_gain_loss) )
 
             portfolio_value += stock_value
             total_gain_loss += stock_gain_loss
@@ -64,5 +76,3 @@ def gain_loss(portfolio_file, prices_file):
 
     print(f'The current portfolio value is ${portfolio_value:,.2f}')
     print(f'The total gain/loss is ${total_gain_loss:,.2f}')
-
-    return price_change
