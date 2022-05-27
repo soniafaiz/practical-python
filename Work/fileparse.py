b0a0f9f494668b0509aff1ac860851d94fbedb2a
soniafaiz <soniafaiz@hotmail.co.uk>
2022-05-27 17:35:43 +0100

    Ex 3.10 modified:   fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,14 +1,14 @@
 # fileparse.py
 #
-# Exercise 3.8
+# Exercise 3.10
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     '''
     Parse a CSV file into a list of records
     '''
 
-    if not has_headers and select:
+    if not silence_errors and not has_headers and select:
         raise RuntimeError('select argument requires column headers.')
 
     with open(filename) as f:
@@ -23,21 +23,26 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
                 indices = list(range(len(headers)))
 
         records = []
-        for row in rows:
+        for rowno, row in enumerate(rows):
             if not row:    # Skip rows with o data
                 continue
-            if has_headers:
-                if types:
-                    record = {headers[i]: func(row[i]) for func, i in zip(types,indices)}
-                else:
-                    record = {headers[i]: row[i] for i in indices}
-            else:
-                if types:
-                    record = tuple([func(val) for func,val in zip(types,row)])
 
+            try:
+                if has_headers:
+                    if types:
+                        record = {headers[i]: func(row[i]) for func, i in zip(types,indices)}
+                    else:
+                        record = {headers[i]: row[i] for i in indices}
                 else:
-                    record = row
+                    if types:
+                        record = tuple([func(val) for func,val in zip(types,row)])
+                    else:
+                        record = row
 
-            records.append(record)
+                records.append(record)
+            except ValueError as e:
+                if not silence_errors:
+                    print(f'Row {rowno}: Couldn\'t convert {row}')
+                    print(f'Row {rowno}: Reason: {e}')
 
     return records
