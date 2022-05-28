cdf1708d50746dec946516957a7f5eb920dfe7ed
soniafaiz <soniafaiz@hotmail.co.uk>
2022-05-28 16:44:46 +0100

    Ex 4.9 modified:   stock.py

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,6 @@
 # stock.py
 #
+# Exercise 4.9
 
 class Stock:
     def __init__(self, name, shares, price):
@@ -12,3 +13,6 @@ class Stock:
 
     def sell(self, sell_shares):
         self.shares -= sell_shares
+
+    def __repr__(self):
+        return f'Stock(\'{self.name}\', {self.shares}, {self.price})'
